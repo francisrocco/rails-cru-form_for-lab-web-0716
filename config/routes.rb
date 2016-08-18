443f8a397343ef1bcef00dc62700190e45e03364
Francis Rocco <roccofrancis@gmail.com>
2016-08-18 17:03:45 -0400

    remove extraneous routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,5 @@
 Rails.application.routes.draw do
-  resources :artists, only: [:new, :create, :show, :edit, :update] do
-    resources :songs, only: [:new, :create]
-  end
-
-  resources :genres, only: [:new, :update, :create, :edit, :show] do
-    resources :songs, only: [:new, :create]
-  end
-
+  resources :artists, only: [:new, :create, :show, :edit, :update]
+  resources :genres, only: [:new, :create, :show, :edit, :update]
   resources :songs, only: [:new, :create, :index, :show, :edit, :update]
 end
